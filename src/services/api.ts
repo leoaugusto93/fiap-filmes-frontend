239e6e2b6e34bf147875c1ae56952487788628c7
Leonardo Augusto <leonardo.augusto.93@outlook.com>
2023-11-15 18:42:51 -0300

    deploy vercel

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:3333'
+    //baseURL: 'http://localhost:3333'
+    baseURL: 'https://fiap-filmes-backend.vercel.app'
 });
 export default api;
